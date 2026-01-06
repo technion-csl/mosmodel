c7d5e35c40a16390613e4c7ba058a2cb2a03e96a
Hosam Morad <hosam5.morad3@gmail.com>
2026-01-06 04:20:53 +0200

    WIP

--- experiments/pebs_tlb_miss_trace/module.mk
+++ experiments/pebs_tlb_miss_trace/module.mk
@@ -12,12 +12,24 @@ PEBS_EXP_OUT_DIR := $(MODULE_NAME)/repeat1
 PEBS_TLB_MISS_TRACE_OUTPUT := $(PEBS_EXP_OUT_DIR)/perf.data
 
 # For PEBS experiments, perf needs to wrap the entire command, so we can't use SET_TASK_AFFINITY_CMD in the prefix
-ifdef CPU_ISOLATION
+$(info ======== PEBS DEBUG ========)
+$(info CPU_ISOLATION = [$(CPU_ISOLATION)])
+$(info CSET_SHIELD_RUN = [$(CSET_SHIELD_RUN)])
+$(info ISOLATED_CPUS = [$(ISOLATED_CPUS)])
+$(info SET_TASK_AFFINITY_CMD = [$(SET_TASK_AFFINITY_CMD)])
+$(info ============================)
+ifdef CSET_SHIELD_RUN
+$(info >>> Taking CSET_SHIELD_RUN branch)
+# cset shield wraps perf, which then wraps the benchmark
+PEBS_PREFIX = $(SET_TASK_AFFINITY_CMD) $(PERF_MEM_RECORD_CMD) --output=$(ROOT_DIR)/$(PEBS_TLB_MISS_TRACE_OUTPUT) -- $(SET_TASK_AFFINITY_CMD)
+else ifdef CPU_ISOLATION
+$(info >>> Taking CPU_ISOLATION branch)
 PEBS_PREFIX = $(PERF_MEM_RECORD_CMD) --output=$(ROOT_DIR)/$(PEBS_TLB_MISS_TRACE_OUTPUT) --cpu $(ISOLATED_CPUS) -- $(SET_TASK_AFFINITY_CMD)
-else # CSET_SHIELD_RUN and SERIAL_RUN
+else
+$(info >>> Taking SERIAL_RUN branch (no isolation))
+# SERIAL_RUN - no CPU affinity, just perf record
 PEBS_PREFIX = $(SET_TASK_AFFINITY_CMD) $(PERF_MEM_RECORD_CMD) --output=$(ROOT_DIR)/$(PEBS_TLB_MISS_TRACE_OUTPUT) -- $(SET_TASK_AFFINITY_CMD)
 endif 
-
 $(PEBS_EXP_OUT_DIR): $(PEBS_TLB_MISS_TRACE_OUTPUT)
 $(MODULE_NAME): $(PEBS_TLB_MISS_TRACE_OUTPUT)
 
